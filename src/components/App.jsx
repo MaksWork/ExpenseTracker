089c80a25285744972dc59f27a415aa32dcd4255
SausageOG <sauageog@gmail.com>
2021-11-07 00:58:12 +0200

    Add transaction done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,8 +8,8 @@ const App = () => {
             <h2>Expense tracker</h2>
             <div className={styles.mainInfo}>
                 <div className={styles.balance}>
-                    <h3 id={styles.balance_title}>Your balance</h3>
-                    <h1 id={styles.balance_title}>$ 180.00</h1>
+                    <h3>Your balance</h3>
+                    <h1>$ 180.00</h1>
                 </div>
                 <div className={styles.income_expense}>
                     <div className={`${styles.income_cont} ${styles.col}`}>
@@ -27,22 +27,37 @@ const App = () => {
             <div className={styles.history}>
                 <div className={styles.trans_list}>
                     <div className={`${styles.trans_list_item} ${styles.trans_expense}`}>
-                        <span className={styles.trans_name}>Book</span>
+                        <button>X</button>
+                        <span>Book</span>
                         <span className={styles.trans_price}>-$ 120.00</span>
                     </div>
+                    <div></div>
                     <div className={`${styles.trans_list_item} ${styles.trans_income}`}>
-                        <span className={styles.trans_name}>Cash</span>
+                        <span>Cash</span>
                         <span className={styles.trans_price}>+$ 300.00</span>
                     </div>
                     <div className={`${styles.trans_list_item} ${styles.trans_expense}`}>
-                        <span className={styles.trans_name}>Book</span>
+                        <span>Book</span>
                         <span className={styles.trans_price}>-$ 120.00</span>
                     </div>
 
                 </div>
             </div>
             <h3 id={styles.block_title}>Add new transaction</h3>
-            <div className={styles.addTransaction}></div>
+            <div className={styles.addTransaction}>
+                <div className={styles.text_cont}>
+                    <span>Text</span>
+                    <input placeholder="Enter text..."></input>
+                </div>
+                <div className={styles.amount_cont}>
+                    <span>Amount
+                    <h4>(negative - expense;positive - income)</h4></span>
+                    <input placeholder="Enter amount..."></input>
+                </div>
+                <div className={styles.btn_cont}>
+                    <button>Add transaction</button>
+                </div>
+            </div>
         </div>
     );
 };
